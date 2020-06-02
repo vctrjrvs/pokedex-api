d7eabac6a4a9ea4b96089cd59c91b29e45733e4b
Victor Jarvis <vctrjrvs@gmail.com>
2020-06-02 17:46:28 -0400

    errors fixed, project resumed!

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
+const helmet = require('helmet')
 const POKEDEX = require('./pokedex.json')
 
 console.log(process.env.API_TOKEN)
 
 const app = express()
 
+app.use(helmet())
+app.use(cors())
 app.use(morgan('dev'))
+
 
 const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`]
 
